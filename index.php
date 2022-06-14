ea27b217d4ff3587771da66452c49a82eaabb82f
Pablo Garcia <PabloEduardoGarcia18@gmail.com>
2022-06-13 21:03:07 -0300

    Creación del Proyecto.
    Creación del primer maquetado y carpetas del Proyecto.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Butique</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="menu-container">
+</div>
+
+    <div class="video">
+    </div>
+
+    <div class="author-container">
+        <div class="author-row">
+            <div class="author-column">
+
+            </div>
+        </div>
+    </div>
+    <section class="products">
+        <div class="box-container">
+
+        </div>
+    </section>
+    
+    <div class="contacto">
+
+</div>
+</body>
+</html>
